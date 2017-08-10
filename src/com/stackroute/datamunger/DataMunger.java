1badb31c160b645ec5f443d861e40d0920c9838e
thrijita <kthrijita@gmail.com>
2017-08-10 17:27:17 +0530

    review

--- src/com/stackroute/datamunger/DataMunger.java
+++ src/com/stackroute/datamunger/DataMunger.java
@@ -1,19 +1,23 @@
 package com.stackroute.datamunger;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class DataMunger {
 
 	public static void main(String[] args) {
 		
-		//creation of object for the current class DataMunger
-		DataMunger dataMunger = new DataMunger();
 		// read the query from the user into queryString variable
-		String queryString="select * from ipl.csv where season > 2014 and city ='Bangalore'";
-		
+		Scanner scanner=new Scanner(System.in);
+		String queryString=scanner.nextLine();
+		DataMunger dataMunger=new DataMunger();
 		// call the parseQuery method and pass the queryString variable as a parameter
+		
 		dataMunger.parseQuery(queryString);
+
+		
 	}
 	
 
@@ -34,86 +38,148 @@ public class DataMunger {
 	// parse the queryString into words and display
 	public String[] getSplitStrings(String queryString) {
 				//split of string according to space
-				queryString = queryString.toLowerCase();
-				String[] splittedString = queryString.split("\\s+");
+				String[] splittedString = queryString.split(" ");
 				return splittedString;
 	}
 
 	// get and display the filename
 	public String getFile(String queryString) {
 				// code to get the file name
-				queryString = queryString.toLowerCase();
-				String fileName = queryString.split("from")[1].split("\\s+")[1];
-				queryString = queryString.toLowerCase();
-				String[] fields = queryString.split("select")[1].trim().split("from")[0].split("[\\s,]+");
-
+				String fileName = queryString.split("from")[1].split("where")[0];
+				//String[] fields = queryString.split("select")[1].trim().split("from")[0].split("[\\s,]+");
 				return fileName;
 	}
 	
 	// getting the baseQuery and display
 	public String getBaseQuery(String queryString) {
 		//getting base query
-		queryString = queryString.toLowerCase();
-		String baseQuery = queryString.split("where |order by|group by")[0];
+		String baseQuery="";
+		if(queryString.contains("where"))
+		{
+		baseQuery=(queryString.split("where")[0].trim());
+		}
+		else if(queryString.contains("group by")||queryString.contains("order by"))
+		{
+			baseQuery=(queryString.split("order by|group by")[0].trim());	
+		}
+		
 		return baseQuery;
+
 	}
 	
 	// get and display the where conditions part(if where condition exists)
 	public String getConditionsPartQuery(String queryString) {
-		
-	
+		//getting only where with order,group by
+		if(queryString.contains("where"))
+		{
+		String conditionsQuery=(queryString.split("where")[1].trim()).split("order by|group by")[0].trim().toLowerCase();
+		return conditionsQuery;
+		}
+		else
+		{
 		return null;
+		}
+
 
 	}
 	
 	/* parse the where conditions and display the propertyName, propertyValue and
 	 conditionalOperator for each conditions*/
 	public String[] getConditions(String queryString) {
-		
-	
-		return null;
+		//where with order,group,and,or
+		if(queryString.contains("where"))
+		{
+		String[] conditions=(((queryString.split("where")[1].trim()).split("order by|group by")[0].trim().toLowerCase().trim()).split(" and | or "));
+		return conditions;
+		}
+		else
+		{
+			return null;	
+		}
+
 	}
 	
 	// get the logical operators(applicable only if multiple conditions exist)
 	public String[] getLogicalOperators(String queryString) {
+		//only logical operators
+		List<String> logicalOperator=new ArrayList<>();
+		if(queryString.contains("where"))
+		{
+		String[] conditionsQuery=((queryString.split("where")[1].trim()).split("order by|group by")[0].trim().toLowerCase()).split(" ");
+		for(String string:conditionsQuery)
+		{
+			if(string.equals("and")||string.equals("or"))
+				logicalOperator.add(string);
+		}
+		String[] logicalOperators=logicalOperator.toArray(new String[logicalOperator.size()]);
+		return logicalOperators;
+		}
+		else
+		{
+			return null;
+		}
 
-		
-		
-		return null;
 		
 	}
 	
 	/*get the fields from the select clause*/
 	public String[] getFields(String queryString) {
 				//split according to select , from and storing separately
-				queryString = queryString.toLowerCase();
-				String[] fields = queryString.split("select")[1].trim().split("from")[0].split("[\\s,]+");
-
+				
+				//String[] fields = queryString.split("select")[1].trim().split("from")[0].split("[\\s,]+");
+				String[] fields=(queryString.split("from")[0].trim()).split("select")[1].trim().split(",");
 				return fields;
+
+				//return fields;
 		
 	}
 	// get order by fields if order by clause exists
 	public String[] getOrderByFields(String queryString) {
 		//code to split order by
-		queryString = queryString.toLowerCase();
-		String [] orderByFields=queryString.split("order by")[1].split("[\\s,]+");
-		return orderByFields;
+		if(queryString.contains("order by"))
+		{
+		String[] orderByField=(queryString.split("order by")[1].trim()).split(" ")[0].trim().toLowerCase().split(" ");
+		return orderByField;
+		}
+		
+		return null;
+
 	}
 	
 	// get group by fields if group by clause exists
 	public String[] getGroupByFields(String queryString) {
 		//code to split group by and to store separately
-		queryString = queryString.toLowerCase();
-		String [] groupByFields=queryString.split("group by\\s+")[1].split("[\\s,]+");	
-
+		if(queryString.contains("group by"))
+		{
+		String[] groupByFields=(queryString.split("group by")[1].trim()).split(" ")[0].trim().toLowerCase().split(" ");
 		return groupByFields;
+		}
+		return null;
+
 	}
 	
 	// parse and display aggregate functions(if applicable)
 	public String[] getAggregateFunctions(String queryString) {
-		
+		//code to split aggregate functions alone
+		List<String> aggregateFunction=new ArrayList<>();
+		if(queryString.contains("("))
+		{
+		for(String string:getFields(queryString))
+		{
+			if(string.contains("min")||string.contains("max")||string.contains("avg")||string.contains("sum")||string.contains("count"))
+			{
+			aggregateFunction.add(string);
+			}
+		}
+		String[] aggregateFunctions=aggregateFunction.toArray(new String[aggregateFunction.size()]);
 
+		return aggregateFunctions;
+		}
+		else
+		{
 		return null;
+		}
+
 	}
 
 	
